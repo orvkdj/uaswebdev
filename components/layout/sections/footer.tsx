73012a83c845ee6716ade23bd1f9525a07149386
rei009 <tokugarit@gmail.com>
2026-01-20 20:41:10 +0700

    farid

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -1,5 +1,5 @@
 import { Separator } from "@/components/ui/separator";
-import { ChevronsDownIcon } from "lucide-react";
+import { HeadphonesIcon } from "lucide-react";
 import Link from "next/link";
 
 export const FooterSection = () => {
@@ -9,35 +9,30 @@ export const FooterSection = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
+              <HeadphonesIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
 
-              <h3 className="text-2xl">Shadcn</h3>
+              <h3 className="text-2xl">BeMusic</h3>
             </Link>
           </div>
 
           <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-lg">Contact</h3>
+            <h3 className="font-bold text-lg">Kontak</h3>
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
+              <Link href="https://github.com/orvkdj/uaswebdev" className="opacity-60 hover:opacity-100">
                 Github
               </Link>
             </div>
 
-            <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Twitter
-              </Link>
-            </div>
 
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
+              <Link href="https://www.instagram.com/bemusiccx?igsh=MXA3enk0c3hmYmU3cg%3D%3D" className="opacity-60 hover:opacity-100">
                 Instagram
               </Link>
             </div>
           </div>
 
           <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-lg">Platforms</h3>
+            <h3 className="font-bold text-lg">Platform</h3>
             <div>
               <Link href="#" className="opacity-60 hover:opacity-100">
                 iOS
@@ -58,7 +53,7 @@ export const FooterSection = () => {
           </div>
 
           <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-lg">Help</h3>
+            <h3 className="font-bold text-lg">Bantuan</h3>
             <div>
               <Link href="#" className="opacity-60 hover:opacity-100">
                 Contact Us
@@ -79,22 +74,22 @@ export const FooterSection = () => {
           </div>
 
           <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-lg">Socials</h3>
+            <h3 className="font-bold text-lg">Sosial</h3>
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
+              <Link href="https://Twitch.com" className="opacity-60 hover:opacity-100">
                 Twitch
               </Link>
             </div>
 
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
+              <Link href="https://discord.gg/HjnH5S2df" className="opacity-60 hover:opacity-100">
                 Discord
               </Link>
             </div>
 
             <div>
-              <Link href="#" className="opacity-60 hover:opacity-100">
-                Dribbble
+              <Link href="https://Dribble.com" className="opacity-60 hover:opacity-100">
+                Dribble
               </Link>
             </div>
           </div>
@@ -103,13 +98,13 @@ export const FooterSection = () => {
         <Separator className="my-6" />
         <section className="">
           <h3 className="">
-            &copy; 2024 Designed and developed by
+            &copy; 2026 Desain dan Dikembangkan Oleh
             <Link
               target="_blank"
               href="https://github.com/leoMirandaa"
               className="text-primary transition-all border-primary hover:border-b-2 ml-1"
             >
-              Leo Miranda
+              Kelompok 3
             </Link>
           </h3>
         </section>
--- components/layout/sections/services.tsx
+++ components/layout/sections/services.tsx
@@ -17,25 +17,25 @@ interface ServiceProps {
 }
 const serviceList: ServiceProps[] = [
   {
-    title: "Custom Domain Integration",
+    title: "Personal Sound",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit adipisicing.",
+      "Musik menyesuaikan suasana dan preferensi setiap penggunacurated discovery Lagu baru dipilih secara cerdas dan relevan dengan seleramu.",
     pro: 0,
   },
   {
-    title: "Social Media Integrations",
+    title: "Seamless Experience",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, dicta.",
+      "Pengalaman mendengarkan terasa konsisten di berbagai perangkat.",
     pro: 0,
   },
   {
-    title: "Email Marketing Integrations",
-    description: "Lorem dolor sit amet adipisicing.",
+    title: "Curated Discovery",
+    description: "Lagu baru dipilih secara cerdas dan relevan dengan seleramu.",
     pro: 0,
   },
   {
-    title: "SEO Optimization",
-    description: "Lorem ipsum dolor sit amet consectetur.",
+    title: "Distraction Free",
+    description: "Dengarkan musik tanpa iklan dan gangguan yang tidak perlu.",
     pro: 1,
   },
 ];
@@ -44,15 +44,15 @@ export const ServicesSection = () => {
   return (
     <section id="services" className="container py-24 sm:py-32">
       <h2 className="text-lg text-primary text-center mb-2 tracking-wider">
-        Services
+        Keunggulan
       </h2>
 
       <h2 className="text-3xl md:text-4xl text-center font-bold mb-4">
-        Grow Your Business
+        Why BeMusic?
       </h2>
       <h3 className="md:w-1/2 mx-auto text-xl text-center text-muted-foreground mb-8">
-        From marketing and sales to operations and strategy, we have the
-        expertise to help you achieve your goals.
+        BeMusic hadir untuk menghadirkan pengalaman mendengarkan musik yang lebih
+        personal, fokus, dan bermakna.
       </h3>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
 
